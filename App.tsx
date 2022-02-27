dec21b867584d32755c56e7cfe3bb1a48c06ef28
Andrej Germič <andrej.germic@moderntv.eu>
2022-02-27 17:02:00 +0100

    feat: add task ui

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import { Task } from "./components/Task"
-import { StyleSheet, Text, View } from "react-native"
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native"
+import React from "react"
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.taskWrapper}>
         <Text style={styles.sectionTitle}>Today's task</Text>
+        {/* Tasks display */}
         <View style={styles.items}>
           <Task>Hello</Task>
           <Task>Do something</Task>
@@ -13,6 +23,21 @@ export default function App() {
           <Task>Great</Task>
         </View>
       </View>
+
+      {/* Task input */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "padding"}
+        style={styles.writeTasksWrapper}
+      >
+        <TextInput style={styles.input} placeholder={"Write a task"} />
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <View style={styles.addText}>
+              <Text>+</Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   )
 }
@@ -33,4 +58,32 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 20,
   },
+  writeTasksWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addText: {},
 })
